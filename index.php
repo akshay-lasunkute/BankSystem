b1064ac0434a3b94818052cbc53c97a832665ada
Akshay Balasaheb Lasunkute <50407592+akshay-lasunkute@users.noreply.github.com>
2021-05-07 11:44:50 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
+    <title>Home</title>
+</head>
+<body>
+    <header>
+        <section class="upperhead">
+           
+            <img id="logoimg" src="img/bank.jpeg" alt="Bank Image">
+           <div id="bankname">
+               <h1>TOD BANK</h1>
+           </div>
+            <img id="logoimg1" src="img/bank.jpeg" alt="Bank Image">
+           
+        </section>
+
+        <nav class="uppernav">
+            <a id="current" href="index.php">HOME</a>
+            <a href="php/aboutus.php">ABOUT US</a>
+        </nav>
+
+    </header>
+
+    <section>
+            <nav class="sidenav">
+                <a href="php/createacc.php">Create Account</a>
+                <a href="php/viewholder.php">View Holders</a>
+                <a href="php/MakeTran.php">Make Transactions</a>
+                <a href="php/ViewTran.php">View Transaction</a>
+            </nav>
+    </section>
+
+    <section class="maincontent">
+        <img id="img/treeimg" src="tree.png" alt="tree">
+
+        <div id="infot">
+            <h2 style="color: lightcoral;">Transactions On Demand Bank</h2>
+            <h3>Your kind of people... Your kind of Bank.</h3>
+            <h3>---Welcome to the most secure Bank in the world!---</h3>
+            <h3>If you are with us then your money are in safe hands.</h3>
+            <h3>Here you can deposit your money whenever you want.</h3>
+            <h3>you can make the transactions at any time to anyone.</h3>
+            <h3>We Will take care of the details.</h3>
+            <h3>Create By :- Akshay Balasaheb Lasunkute</h3>
+        </div>
+    </section>
+
+
+    <footer>
+
+    </footer>
+</body>
+</html>
